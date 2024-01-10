54525e1d143fb0c053faf0e4d98923dfeb555c7a
SUJAL LUHAR <luharsujal2712@gmail.com>
2024-01-10 16:46:56 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project about maximizing the returns of stock market by optimizing the portfolio and minimizing the risks by applying risk management techniques.',
+    author='Sujal Luhar',
+    license='MIT',
+)
